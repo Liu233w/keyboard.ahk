e4adf90329f46887fe5c29c8f01eb32c9b1ececb
Liu233w <gh@liu233w.com>
2022-03-14 16:47:35 +1100

    remove single key command

--- generate.py
+++ generate.py
@@ -23,49 +23,6 @@ with open('tail.template', 'r', encoding='utf-8') as alias:
 #格式：一个元组的列表，第一项是要按下的按键，第二项是一个字符串，表示要运行的
 #      命令
 single_key = [
-('o', """
-SendInput, {End}
-SendInput, {Enter}
-"""),
-('e', """
-;确定可弹出的驱动器列表
-DriveList = Empty
-DriveGet, DriveList, List
-
-RemovableDriveList := ""
-Loop, Parse, DriveList
-{
-    pd := PhysicalFromLogical(A_LoopField)
-    if GetType(pd) = "Removable" or GetInterface(pd) = "USB"
-        RemovableDriveList := RemovableDriveList A_LoopField
-}
-
-if RemovableDriveList
-    goto, start_eject
-
-MsgBox, 没有需要弹出的驱动器
-return
-
-start_eject:
-
-InputBox, Driveletter, 请输入要弹出的盘符 , %RemovableDriveList%
-if ErrorLevel=1
-    return
-
-StringUpper, Driveletter, Driveletter ;将用户输入的盘符转换成大写
-
-IfInString, Driveletter, %RemovableDriveList%
-{
-    Driveletter = %Driveletter%:
-    Runwait, RemoveDrive.exe %Driveletter%
-
-}
-else
-{
-    Msgbox, 请输入一个列表中的盘符
-    goto, start_eject
-}
-""")
 ]
 
 #多重按键，此列表中的按键有多个映射
